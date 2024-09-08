760e79194d82d885f605f07c847d3d63b6d8bcc5
Jacek <jacuskozlowski@gmail.com>
2024-09-08 19:05:37 +0200

    chore: added gitignore file

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const http = require('http'); // import files to node.js
 
 const server = http.createServer((req, res) => {
     console.log(req);
-}); // node will execute this function whenever request reaches our server
+}); // node will execute this function whenever request reaches our server (ex. we type localhost:3000 in our browser)
 
 server.listen(3000); // port 3000
