5620eaba0efacc17935360eb87403dd72469be22
Jacek <jacuskozlowski@gmail.com>
2024-09-09 19:44:57 +0200

    chore: deleted routes.js on branch 'expand'

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const http = require('http'); 
 
-const routes = require('./routes');
-
-const server = http.createServer(routes.handler);
+const server = http.createServer();
 
 server.listen(3000); 
