ee0c4a1476990de3e6b84852d41de3dba1531538
Jacek <jacuskozlowski@gmail.com>
2024-09-09 20:46:14 +0200

    feat: added express.js to the project

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-const http = require('http'); 
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer();
+const app = express();
 
-server.listen(3000); 
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/name', (req, res, next) => { // middleware function
+    console.log(req.body);
+    res.send('<h1>Hello!</h1>')
+});
+
+app.use('/', (req, res, next) => { // middleware function
+    res.send('<form action="/name" method="POST"><input type="text" name="name"><button type="submit">Whats your name</button></form>');
+});
+
+app.listen(3000);
